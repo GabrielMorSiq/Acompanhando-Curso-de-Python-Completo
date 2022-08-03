263412138e1f661905866c1f4f6e129a4bcc938a
GabrielMorSiq <109690729+GabrielMorSiq@users.noreply.github.com>
2022-08-02 23:48:06 -0300

    Add files via upload

---
+++ 1 Python Básico  - Lógica de Programação/Aula 1 Comentários/Aula 1.py
@@ -0,0 +1,38 @@
+# Aula 1 - Comentários em Python
+
+
+print('Linha 1')
+print('Linha 2')
+# falando do que se trata essa linha
+print('Linha 3')
+print('Linha 4')
+
+'''''
+python é interpretado, não compilado
+esquerda da direita, cima para baixo, pulando cerquilha #
+dois espaços antes de um comentário de linha
+usar comentários para explicar como vc resolveu algo, depois de dias
+talvez vc não se recorde de tal ao abrir o código dias depois
+mas não comentar nada óbvio
+'''''
+print("oi")
+"""
+' é aspas
+" é aspas duplas
+"""
+
+# não quero exibir a linha 5
+# print('Linha 5')
+
+
+# comentário por linha
+print('Oi')
+""""
+Comentario via aspas duplas
+"""
+print('Oi')
+'''
+Comentário via aspas
+'''
+print('0i')
+# print('Oi') - inibindo comando
