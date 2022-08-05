25f4370b781c08d24047cacdbf1e82d1bd2aaf97
GabrielMorSiq <gabrie.moreirasiqueira@gmail.com>
2022-08-04 23:41:54 -0300

    Update

--- 1 Lógica de Programação/Aula 20 - Repetição com acumuladores /Aula 20 While Else.py
+++ 1 Lógica de Programação/Aula 20 - Repetição com acumuladores /Aula 20 While Else.py
@@ -1,6 +1,12 @@
 contador = 1
 acumulador = 1
 
+
+"""
+Acontecerá um break sem que a condição do laço seja falsa. 
+Atingir o else do while com tal verdadeiro... 
+Ele não é executado, só seria quando tal fosse falso 
+"""
 while contador <= 10:
     print(contador, acumulador)
 
@@ -12,8 +18,4 @@ while contador <= 10:
 else:
     print('Cheguei no Else mas não executo')
 
-"""
-Acontecerá um break sem que a condição do laço seja falsa. 
-Atingir o else do while com tal verdadeiro... 
-Ele não é executado, só seria quando tal fosse falso 
-"""
+
